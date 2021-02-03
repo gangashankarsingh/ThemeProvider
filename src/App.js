3cbb50cd79ff803061b46b1e72dcb9d59aefa284
Gangashankar <bigdatamonk@gmail.com>
2021-02-03 08:56:49 +0530

    Added App and index changes while correcting theme-context changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {ThemeContext} from './theme-context'
+import React from 'react'
 
 function App() {
+
+const {theme, toggle, dark } = React.useContext(ThemeContext)
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style = {{backgroundColor: theme.backgroundColor, color: theme.color}} >
+        <button
+        type = "button"
+        onClick = {toggle}
+        style = {{backgroundColor: theme.backgroundColor, color: theme.color}}
+        >
+          Click here to toggle to {!dark ? 'dark': 'light'} theme
+          </button>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/theme-context.js
+++ src/theme-context.js
@@ -49,3 +49,5 @@ return (
 
 
 }
+
+export {ThemeProvider , ThemeContext}
